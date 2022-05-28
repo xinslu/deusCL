e9a9e947b209114fbb7aa9c6c88b7061ecbf3806
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-28 19:14:23 -0400

    Added some stuff but no work bruh

--- code/src/environment.rs
+++ code/src/environment.rs
@@ -1,16 +1,41 @@
 use std::collections::HashMap;
 use std::fmt::Display;
 
-pub struct Environment<'a> {
-    map: HashMap<&'a str, Box<dyn Display + 'static>>,
-    enclosing: Box<Option<Environment<'a>>>
+pub use crate::token:: {
+    Token
+};
+pub struct Environment {
+    map: HashMap<String, Box<dyn Display + 'static>>,
+    enclosing: Box<Option<Environment>>
 }
 
-impl Environment<'_> {
-    pub fn create(enclosing: Option<Environment>) -> Environment{
+impl Environment {
+    pub fn new(enclosing: Option<Environment>) -> Environment{
         Environment {
             map: HashMap::new(),
             enclosing: Box::new(enclosing)
         }
+    }
+
+    pub fn define<T: 'static>(&mut self, name: String, value: T) where T:  Display {
+        self.map.insert(name, Box::new(value));
+    }
+
+
+    pub fn get(&mut self, name: String) -> Box<dyn Display + '_> {
+        match self.map.get(&name) {
+            Some(value) => return Box::new(value),
+            None => {
+                match *self.enclosing {
+                    Some(_) => {
+                        return (*self.enclosing).as_ref().unwrap().get(name);
+                    },
+                    None => {
+                        panic!("Variable not Defined")
+                    }
+                }
+                }
+            }
+
     }
 }
