f9c030d7089f8c19b4a4cbeaf23954896c9211c4
kinshukphalke <phalkekinshuk@gmail.com>
2022-06-19 16:50:51 -0400

    Added WASM Modules

--- code/src/repl.rs
+++ code/src/repl.rs
@@ -1,58 +1,41 @@
 use crate::interpreter::Interpreter;
 use crate::parser::Parser;
 use crate::tokenizer::Tokenizer;
-use log;
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
-use std::panic;
-
+use std::io::{stdin,stdout,Write};
 pub fn repl() {
-    let mut rl = Editor::<()>::new();
     let mut _interpreter = Interpreter::new();
-    if rl.load_history("history.txt").is_err() {
-        println!("No previous history.");
-    }
     loop {
-        let readline = rl.readline("DEUS-USER> ");
         let mut tokenizer = Tokenizer::new();
-        match readline {
+        let mut s=String::new();
+        print!("DEUS_USER> ");
+        let _=stdout().flush();
+        match stdin().read_line(&mut s) {
             Ok(line) => {
-                let processed_line = str::replace(line.as_str(), "\n", " ");
-                rl.add_history_entry(processed_line.clone());
+                let processed_line = str::replace(s.as_str(), "\n", " ");
                 if let Err(error) = tokenizer.tokenize(processed_line.to_string()) {
                     println!("{}", error);
                 } else {
-                // println!("created parser");
-                // tokenizer.print_tokens();
-                // println!("right after print");
-                match Parser::new(tokenizer.tokens).parse() {
-                    Ok(parserresult) => {
-                        // println!("{:?}", parserresult);
-                        if let Err(error) = _interpreter.accept(parserresult.clone()) {
-                            println!("{}", error);
+                    // println!("created parser");
+                    // tokenizer.print_tokens();
+                    // println!("right after print");
+                    match Parser::new(tokenizer.tokens).parse() {
+                        Ok(parserresult) => {
+                            // println!("{:?}", parserresult);
+                            if let Err(error) = _interpreter.accept(parserresult.clone()) {
+                                println!("{}", error);
+                            }
+                            _interpreter.clean_env();
                         }
-                        _interpreter.clean_env();
-                    }
-                    Err(error) => {
-                        println!("{:?}", error);
+                        Err(error) => {
+                            println!("{:?}", error);
+                        }
                     }
                 }
-                }
-
             }
-            Err(ReadlineError::Interrupted) => {
-                log::info!("Bye!!");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                log::info!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                log::warn!("Error: {:?}", err);
+            _ => {
+                println!("Exiting");
                 break;
             }
         }
     }
-    rl.save_history("history.txt").unwrap();
 }
