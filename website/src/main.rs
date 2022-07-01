bb4f564a367b6c78243cbc6343e57626dbfb9d60
kinshukphalke <phalkekinshuk@gmail.com>
2022-07-01 18:42:09 -0400

    Trying to make multi-line work

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,31 +1,39 @@
 use yew::TargetCast;
 use yew::{events::KeyboardEvent, html, Component, Context, Html};
 use web_sys;
-use weblog::{console_log};
+use weblog::console_log;
+use code::repl;
+use yew::prelude::*;
+
 
 enum Msg {
     Submit,
     OnInput(String)
 }
 
+
+#[derive(PartialEq, Default, Debug, Clone)]
 struct Input {
     line: i32,
-    code: String
+    code: String,
+    result: String
 }
 
 impl Component for Input {
-
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { line: 0 , code: "".to_string()}
+        Self { line: 0 , code: "".to_string(), result: "".to_string()}
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Submit => {
-               console_log!("Hello world!", self.line, self.code.clone());
+               console_log!(self.line, self.code.clone());
+               let result = repl::repl(self.code.clone());
+               console_log!(result.clone());
+               self.result = result.clone();
                 true
             },
             Msg::OnInput(value) => {
@@ -53,10 +61,48 @@ impl Component for Input {
                     let input: web_sys::HtmlInputElement = e.target_unchecked_into();
                     Msg::OnInput( input.value() ) })} />
                 </h4>
+                 <h3 id="result0">{self.result.clone()}</h3>
             </>
         }
     }
 }
+
+#[derive(Default, PartialEq, Properties)]
+pub struct ListProps {
+    pub children: Input,
+}
+
+
+enum PMsg {
+    AddLine,
+}
+
+struct Repl {
+    lines: Vec<Input>,
+}
+
+impl Component for Repl {
+    type Message = PMsg;
+    type Properties = ListProps;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { lines: Vec::new()}
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                 {for self.lines.clone().into_iter().map(|line| {
+                    line.view()
+                 })}
+            </div>
+        }
+    }
+
+}
+
 
 
 fn main() {
